a5d0f3f32bd61d5def694cb28dcf6292abfe3fcd
Romain Maury <rmaury@student.42.fr>
2018-01-18 14:12:18 +0100

    rename ft_p.h by server.h

--- src/main.c
+++ src/main.c
@@ -6,6 +6,27 @@
 /*   By: rmaury <rmaury@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 17:37:58 by rmaury            #+#    #+#             */
-/*   Updated: 2018/01/17 17:38:32 by rmaury           ###   ########.fr       */
+/*   Updated: 2018/01/18 14:11:12 by rmaury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// DEBUG INCLUDES >> to delete
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// END DEBUG INCLUDES >> to delete
+
+#include "server.h"
+
+int main(int argc, char const **argv)
+{
+	printf("%s\n", argv[1]);
+	printf("%i\n", atoi(argv[0]));
+	atoi(argv[1]);
+	
+	if (argc != 2 || *argv[1])
+	{
+
+	}
+	return (0);
+}
