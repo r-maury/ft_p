27625142a65b11f7c1e62401baa2bf7528394e59
Romain Maury <rmaury@student.42.fr>
2018-01-23 20:25:33 +0100

    init loop with accept

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: rmaury <rmaury@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 17:37:58 by rmaury            #+#    #+#             */
-/*   Updated: 2018/01/23 19:53:12 by rmaury           ###   ########.fr       */
+/*   Updated: 2018/01/23 20:25:12 by rmaury           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,26 @@
 int main(int argc, char const **argv)
 {
 	int sock_fd;
+	int reuse;
 	struct sockaddr_in server_addr;
+
+	reuse = 1;
+
 	if (argc != 2) 
 	{
 		ft_putendl_fd(ft_strjoin("Usage: ", ft_strjoin(argv[0], " port")), 2);
 		return(0);
 	}
+
 	sock_fd = socket(PF_INET, SOCK_STREAM, 0);
 	setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
-	bind(sock,(struct sockaddr*) &server_address, sizeof(server_address));
-	listen(sock_fd, 5)
+	bind(sock_fd,(struct sockaddr*) &server_addr, sizeof(server_addr));
+	listen(sock_fd, 5);
 
-	// if (sock_fd == -1)
-	// {
-	// 	ft_putstr("Error:");
-	// 	ft_putendl(ft_itoa(errno));
-	// }
-	// else
-	// {
-	// 	ft_putstr("Socket created in fd:");
-	// 	ft_putendl(ft_itoa(sock_fd));
-	// }
+	while(42)
+	{
+		accept(sock_fd, *client_addr, client_len);
+	}
+
 	return(0);
 }
